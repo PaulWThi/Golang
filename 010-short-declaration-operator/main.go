925b94931ab41dab3a36532a6332e8f988192e0e
paul thi <paulthi@pauls-MacBook-Pro.local>
2019-02-15 23:32:36 -0500

    Short declaration operator

--- 010-short-declaration-operator/main.go
+++ 010-short-declaration-operator/main.go
@@ -1,1 +1,20 @@
-package _10_short_declaration_operator
+package main
+
+import (
+	"fmt"
+)
+
+var y = 10
+var z int
+
+func main() {
+	x := 20
+	fmt.Println(x)
+	fmt.Println(y)
+	foo()
+	z = 30
+	fmt.Println(z)
+}
+func foo() {
+	fmt.Println("again: ", y)
+}
