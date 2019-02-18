272900a8e911d10c935b71a958f55255ddad6777
paul thi <paulthi@pauls-MacBook-Pro.local>
2019-02-18 10:18:10 -0500

    Fix directories, Add 012-exploring-type and 013-zero-value

---
+++ 012-exploring-type/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var y = 42
+var z string = "Hello World!"
+var a string = `Paul said, "Hello World!"`
+
+// Same as 011-var-keyword
+func main() {
+
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+}
